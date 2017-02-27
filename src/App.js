b60e87882b8518cd1a41aa8abd5869f632d25303
Steve Kinney <hello@stevekinney.net>
2017-02-27 16:50:28 -0700

    Implement writing to the database

--- src/App.js
+++ src/App.js
@@ -6,16 +6,33 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      newData: ''
     };
+
+    this.dataRef = firebase.database().ref();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentWillMount() {
-    firebase.database().ref().on('value', (snapshot) => {
+    this.dataRef.on('value', (snapshot) => {
       this.setState({
         data: snapshot.val()
       });
     });
+  }
+
+  handleChange(event) {
+    this.setState({
+      newData: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.dataRef.push(this.state.newData);
+    this.setState({ newData: '' });
   }
 
   render() {
@@ -27,6 +44,10 @@ class App extends Component {
         <pre className="App-data">
           { JSON.stringify(this.state.data, null, 2) }
         </pre>
+        <form className="App-form" onSubmit={this.handleSubmit}>
+          <input value={this.state.newData} onChange={this.handleChange} />
+          <input type="submit" disabled={!this.state.newData} />
+        </form>
       </div>
     );
   }
