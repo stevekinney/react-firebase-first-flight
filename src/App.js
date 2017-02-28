19a2b06734226a40fd4006024c69b78968c842bb
Steve Kinney <hello@stevekinney.net>
2017-02-27 18:00:44 -0700

    Get basic project set up

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
 import React, { Component } from 'react';
-import firebase from './firebase';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      newData: ''
-    };
-
-    this.dataRef = firebase.database().ref();
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  componentWillMount() {
-    this.dataRef.on('value', (snapshot) => {
-      this.setState({
-        data: snapshot.val()
-      });
-    });
-  }
-
-  handleChange(event) {
-    this.setState({
-      newData: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.dataRef.push(this.state.newData);
-    this.setState({ newData: '' });
-  }
-
   render() {
     return (
       <div className="App">
@@ -42,12 +9,8 @@ class App extends Component {
           <h2>Welcome to React and Firebase</h2>
         </div>
         <pre className="App-data">
-          { JSON.stringify(this.state.data, null, 2) }
+          One day, some data from Firebase will go here.
         </pre>
-        <form className="App-form" onSubmit={this.handleSubmit}>
-          <input value={this.state.newData} onChange={this.handleChange} />
-          <input type="submit" disabled={!this.state.newData} />
-        </form>
       </div>
     );
   }
