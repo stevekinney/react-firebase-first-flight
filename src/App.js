fd1775618e4f1d8beaac67ea58c349ddaccc52c3
Steve Kinney <hello@stevekinney.net>
2017-03-09 15:33:25 -0600

    Update class names

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App--header">
           <h2>Welcome to React and Firebase</h2>
         </div>
-        <pre className="App-data">
+        <pre className="App--data">
           One day, some data from Firebase will go here.
         </pre>
       </div>
