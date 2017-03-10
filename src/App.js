9d68b850164b3b32d2d3d37be6e3dc6d54e0db11
Steve Kinney <hello@stevekinney.net>
2017-03-10 10:48:46 -0600

    Get the database to update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react';
+import { database } from './firebase';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      newData: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    database.ref().on('value', (snapshot) => {
+      console.log('Hello!');
+      this.setState({
+        data: snapshot.val()
+      });
+    });
+  }
+
+  handleChange(event) {
+    const newData = event.target.value;
+    this.setState({
+      newData
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    database.ref()
+            .child('AMAZINGNEWDATA')
+            .set(this.state.newData);
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,8 +44,12 @@ class App extends Component {
           <h2>Welcome to React and Firebase</h2>
         </div>
         <pre className="App--data">
-          One day, some data from Firebase will go here.
+          { JSON.stringify(this.state.data, null, 2) }
         </pre>
+        <form className="App--form" onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.newData} onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
